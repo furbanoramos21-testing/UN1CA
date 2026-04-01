07eba5125fc3f4c8f21d9b7bd0985b2223d3aa16
furbanoramos21 <107002046+furbanoramos21-testing@users.noreply.github.com>
2026-04-01 04:43:48 -0500

    s5e3830: blobs: add libMultiFrameProcessing10 from a14nsxx
    Required by some features on samsung camera on most exynos 850 devices.

--- platform/s5e3830/patches/blobs/customize.sh
+++ platform/s5e3830/patches/blobs/customize.sh
@@ -46,3 +46,7 @@ LOG_STEP_OUT
 
 # Radio FM jni blob
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/lib64/libfmradio_jni.so" 0 0 644 "u:object_r:system_lib_file:s0"
+
+# MultiFrameProcessing10 blob for camera
+ADD_TO_WORK_DIR "a14nsxx" "system" "system/lib64/libMultiFrameProcessing10.camera.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+EVAL "echo \"libMultiFrameProcessing10.camera.samsung.so\" >> \"$WORK_DIR/system/system/etc/public.libraries-camera.samsung.txt\""
