d0b6bb4561429b0b2a7332725c84868a1337c766
furbanoramos21 <107002046+furbanoramos21-testing@users.noreply.github.com>
2026-09-21 18:46:30 +0000

    [TMP] a21s: patches: ril: handle ril files dynamically
    Also added lib64/android.system.keystore2-V1-ndk.so just to be sure, no idea in what this lib is related

--- platform/s5e3830/patches/ril/customize.sh
+++ platform/s5e3830/patches/ril/customize.sh
@@ -1,18 +1,23 @@
 if [ "$TARGET_PLATFORM_SDK_VERSION" -lt "34" ]; then
     # Delete old files
-    DELETE_FROM_WORK_DIR "vendor" "lib64/android.hardware.radio.config@1.0.so"
-    DELETE_FROM_WORK_DIR "vendor" "lib64/android.hardware.radio.config@1.1.so"
-    DELETE_FROM_WORK_DIR "vendor" "lib64/android.hardware.radio.config@1.2.so"
-    DELETE_FROM_WORK_DIR "vendor" "lib64/android.hardware.radio.deprecated@1.0.so"
-    DELETE_FROM_WORK_DIR "vendor" "lib64/android.hardware.radio@1.3.so"
-    DELETE_FROM_WORK_DIR "vendor" "lib64/android.hardware.radio@1.4.so"
-    DELETE_FROM_WORK_DIR "vendor" "lib64/android.hardware.radio@1.5.so"
-    DELETE_FROM_WORK_DIR "vendor" "lib64/vendor.samsung.hardware.radio.bridge@2.0.so"
-    DELETE_FROM_WORK_DIR "vendor" "lib64/vendor.samsung.hardware.radio.bridge@2.1.so"
-    DELETE_FROM_WORK_DIR "vendor" "lib64/vendor.samsung.hardware.radio.channel@2.0.so"
-    DELETE_FROM_WORK_DIR "vendor" "lib64/vendor.samsung.hardware.radio@2.0.so"
-    DELETE_FROM_WORK_DIR "vendor" "lib64/vendor.samsung.hardware.radio@2.1.so"
-    DELETE_FROM_WORK_DIR "vendor" "lib64/vendor.samsung.hardware.radio@2.2.so"
+    BLOBS_LIST="
+    android.hardware.radio.config@1.0.so
+    android.hardware.radio.config@1.1.so
+    android.hardware.radio.config@1.2.so
+    android.hardware.radio.deprecated@1.0.so
+    android.hardware.radio@1.3.so
+    android.hardware.radio@1.4.so
+    android.hardware.radio@1.5.so
+    vendor.samsung.hardware.radio.bridge@2.0.so
+    vendor.samsung.hardware.radio.bridge@2.1.so
+    vendor.samsung.hardware.radio.channel@2.0.so
+    vendor.samsung.hardware.radio@2.0.so
+    vendor.samsung.hardware.radio@2.1.so
+    vendor.samsung.hardware.radio@2.2.so
+    "
+    for blobs in $BLOBS_LIST; do
+      DELETE_FROM_WORK_DIR "vendor" "lib64/$blobs"
+    done
     
     # /vendor/bin/hw
     EVAL "cp -f \"$MODPATH/bin/hw/rild\" \"$WORK_DIR/vendor/bin/hw/rild\""
@@ -30,59 +35,38 @@ if [ "$TARGET_PLATFORM_SDK_VERSION" -lt "34" ]; then
     SET_METADATA "vendor" "etc/vintf/manifest.xml" 0 0 644 "u:object_r:vendor_configs_file:s0"
     
     # /vendor/lib64
-    EVAL "cp -f \"$MODPATH/lib64/android.hardware.radio@1.0.so\" \"$WORK_DIR/vendor/lib64/android.hardware.radio@1.0.so\""
-    EVAL "cp -f \"$MODPATH/lib64/android.hardware.radio@1.1.so\" \"$WORK_DIR/vendor/lib64/android.hardware.radio@1.1.so\""
-    EVAL "cp -f \"$MODPATH/lib64/android.hardware.radio@1.2.so\" \"$WORK_DIR/vendor/lib64/android.hardware.radio@1.2.so\""
-    EVAL "cp -f \"$MODPATH/lib64/android.hardware.radio.config-V1-ndk.so\" \"$WORK_DIR/vendor/lib64/android.hardware.radio.config-V1-ndk.so\""
-    EVAL "cp -f \"$MODPATH/lib64/android.hardware.radio.data-V1-ndk.so\" \"$WORK_DIR/vendor/lib64/android.hardware.radio.data-V1-ndk.so\""
-    EVAL "cp -f \"$MODPATH/lib64/android.hardware.radio.messaging-V1-ndk.so\" \"$WORK_DIR/vendor/lib64/android.hardware.radio.messaging-V1-ndk.so\""
-    EVAL "cp -f \"$MODPATH/lib64/android.hardware.radio.modem-V1-ndk.so\" \"$WORK_DIR/vendor/lib64/android.hardware.radio.modem-V1-ndk.so\""
-    EVAL "cp -f \"$MODPATH/lib64/android.hardware.radio.network-V1-ndk.so\" \"$WORK_DIR/vendor/lib64/android.hardware.radio.network-V1-ndk.so\""
-    EVAL "cp -f \"$MODPATH/lib64/android.hardware.radio.sim-V1-ndk.so\" \"$WORK_DIR/vendor/lib64/android.hardware.radio.sim-V1-ndk.so\""
-    EVAL "cp -f \"$MODPATH/lib64/android.hardware.radio-V1-ndk.so\" \"$WORK_DIR/vendor/lib64/android.hardware.radio-V1-ndk.so\""
-    EVAL "cp -f \"$MODPATH/lib64/android.hardware.radio.voice-V1-ndk.so\" \"$WORK_DIR/vendor/lib64/android.hardware.radio.voice-V1-ndk.so\""
-    EVAL "cp -f \"$MODPATH/lib64/libprotobuf-cpp-full-21.7.so\" \"$WORK_DIR/vendor/lib64/libprotobuf-cpp-full-21.7.so\""
-    EVAL "cp -f \"$MODPATH/lib64/libprotobuf-cpp-lite-21.7.so\" \"$WORK_DIR/vendor/lib64/libprotobuf-cpp-lite-21.7.so\""
-    EVAL "cp -f \"$MODPATH/lib64/libril_sem.so\" \"$WORK_DIR/vendor/lib64/libril_sem.so\""
-    EVAL "cp -f \"$MODPATH/lib64/librilutils.so\" \"$WORK_DIR/vendor/lib64/librilutils.so\""
-    EVAL "cp -f \"$MODPATH/lib64/libsecril-client.so\" \"$WORK_DIR/vendor/lib64/libsecril-client.so\""
-    EVAL "cp -f \"$MODPATH/lib64/libsec-ril.so\" \"$WORK_DIR/vendor/lib64/libsec-ril.so\""
-    EVAL "cp -f \"$MODPATH/lib64/libSemDataProps.so\" \"$WORK_DIR/vendor/lib64/libSemDataProps.so\""
-    EVAL "cp -f \"$MODPATH/lib64/libSemTelephonyProps.so\" \"$WORK_DIR/vendor/lib64/libSemTelephonyProps.so\""
-    EVAL "cp -f \"$MODPATH/lib64/vendor.samsung.hardware.radio.bridge-V1-ndk.so\" \"$WORK_DIR/vendor/lib64/vendor.samsung.hardware.radio.bridge-V1-ndk.so\""
-    EVAL "cp -f \"$MODPATH/lib64/vendor.samsung.hardware.radio.channel-V1-ndk.so\" \"$WORK_DIR/vendor/lib64/vendor.samsung.hardware.radio.channel-V1-ndk.so\""
-    EVAL "cp -f \"$MODPATH/lib64/vendor.samsung.hardware.radio.data-V1-ndk.so\" \"$WORK_DIR/vendor/lib64/vendor.samsung.hardware.radio.data-V1-ndk.so\""
-    EVAL "cp -f \"$MODPATH/lib64/vendor.samsung.hardware.radio.messaging-V1-ndk.so\" \"$WORK_DIR/vendor/lib64/vendor.samsung.hardware.radio.messaging-V1-ndk.so\""
-    EVAL "cp -f \"$MODPATH/lib64/vendor.samsung.hardware.radio.network-V1-ndk.so\" \"$WORK_DIR/vendor/lib64/vendor.samsung.hardware.radio.network-V1-ndk.so\""
-    EVAL "cp -f \"$MODPATH/lib64/vendor.samsung.hardware.radio.sim-V1-ndk.so\" \"$WORK_DIR/vendor/lib64/vendor.samsung.hardware.radio.sim-V1-ndk.so\""
-    EVAL "cp -f \"$MODPATH/lib64/vendor.samsung.hardware.radio-V1-ndk.so\" \"$WORK_DIR/vendor/lib64/vendor.samsung.hardware.radio-V1-ndk.so\""
-    SET_METADATA "vendor" "lib64/android.hardware.radio@1.0.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/android.hardware.radio@1.1.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/android.hardware.radio@1.2.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/android.hardware.radio.config-V1-ndk.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/android.hardware.radio.data-V1-ndk.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/android.hardware.radio.messaging-V1-ndk.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/android.hardware.radio.modem-V1-ndk.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/android.hardware.radio.network-V1-ndk.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/android.hardware.radio.sim-V1-ndk.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/android.hardware.radio-V1-ndk.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/android.hardware.radio.voice-V1-ndk.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/libprotobuf-cpp-full-21.7.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/libprotobuf-cpp-lite-21.7.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/libril_sem.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/librilutils.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/libsecril-client.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/libsec-ril.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/libSemDataProps.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/libSemTelephonyProps.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/vendor.samsung.hardware.radio.bridge-V1-ndk.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/vendor.samsung.hardware.radio.channel-V1-ndk.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/vendor.samsung.hardware.radio.data-V1-ndk.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/vendor.samsung.hardware.radio.messaging-V1-ndk.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/vendor.samsung.hardware.radio.network-V1-ndk.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/vendor.samsung.hardware.radio.sim-V1-ndk.so" 0 0 644 "u:object_r:vendor_file:s0"
-    SET_METADATA "vendor" "lib64/vendor.samsung.hardware.radio-V1-ndk.so" 0 0 644 "u:object_r:vendor_file:s0"
-    
+    LIBS_LIST="
+    lib64/android.hardware.radio.config-V1-ndk.so
+    lib64/android.hardware.radio.data-V1-ndk.so
+    lib64/android.hardware.radio.messaging-V1-ndk.so
+    lib64/android.hardware.radio.modem-V1-ndk.so
+    lib64/android.hardware.radio.network-V1-ndk.so
+    lib64/android.hardware.radio.sim-V1-ndk.so
+    lib64/android.hardware.radio-V1-ndk.so
+    lib64/android.hardware.radio.voice-V1-ndk.so
+    lib64/libprotobuf-cpp-full-21.7.so
+    lib64/libprotobuf-cpp-lite-21.7.so
+    lib64/libril_sem.so
+    lib64/libsec-ril.so
+    lib64/libSemDataProps.so
+    lib64/libSemTelephonyProps.so
+    lib64/vendor.samsung.hardware.radio.bridge-V1-ndk.so
+    lib64/vendor.samsung.hardware.radio.channel-V1-ndk.so
+    lib64/vendor.samsung.hardware.radio.data-V1-ndk.so
+    lib64/vendor.samsung.hardware.radio.messaging-V1-ndk.so
+    lib64/vendor.samsung.hardware.radio.network-V1-ndk.so
+    lib64/vendor.samsung.hardware.radio.sim-V1-ndk.so
+    lib64/vendor.samsung.hardware.radio-V1-ndk.so
+    "
+    if [[ "$TARGET_BOARD_API_LEVEL" == "31" ]]; then
+    LIBS_LIST+="lib64/android.system.keystore2-V1-ndk.so"
+    fi
+
+    for lib in $LIBS_LIST; do
+      EVAL "cp -f \"$MODPATH/$lib\" \"$WORK_DIR/vendor/$lib\""
+      SET_METADATA "vendor" "$lib" 0 0 644 "u:object_r:vendor_file:s0"
+    done
+        
     # Dual SIM fix - not required for UN1CA but it is for a16+
     EVAL "echo \"    setprop ro.telephony.sim_slots.count 2\" >> \"$WORK_DIR/vendor/etc/init/init.baseband.rc\""
     
